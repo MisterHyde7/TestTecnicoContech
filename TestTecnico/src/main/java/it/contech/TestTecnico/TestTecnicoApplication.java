fbd2869c4b16a67f1d01a5ec1ea69fecf90a5b3e
luca.martucci <luca.martucci@accenture.com>
2023-03-08 14:21:56 +0100

    Initial commit

---
+++ TestTecnico/src/main/java/it/contech/TestTecnico/TestTecnicoApplication.java
@@ -0,0 +1,13 @@
+package it.contech.TestTecnico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTecnicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestTecnicoApplication.class, args);
+	}
+
+}
